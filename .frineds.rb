1dd5a32d7cbd9c13eb43a393ef08344036d22c19
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-01-04 16:05:03 -0700

    add elsif statement

--- .frineds.rb
+++ .frineds.rb
@@ -1,8 +1,18 @@
-best_friend = "ryan"
+best_friend = "stephen"
+# good_friend = "Halie"
 
 if best_friend == "ryan"
     puts "hey, how are you?"
+
+elsif best_friend == "Halie"
+    puts "Hello there, Halie"
+
 else
     puts "hey you not who i thought you were"
 
+
 end    
+
+
+
+
